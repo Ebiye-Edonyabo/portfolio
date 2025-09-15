e18abf7351d602ce9bff5c55af1954ec333e6878
Ebiye-Edonyabo <edonyaboebiye11@gmail.com>
2025-09-15 01:53:22 +0100

    blur navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+document.addEventListener('scroll', function () {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 20) {
+        nav.classList.add('backdrop-blur-md',);
+    } else {
+        nav.classList.remove('backdrop-blur-md',);
+    }
+});
